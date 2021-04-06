fb7e0fcb1d53634f98839338290e522cf34dc611
Ethan <ethanchristianwayne@gmail.com>
2021-04-06 14:16:29 -0500

    commit tree e64d3af0271c0f79c79c669391330ddf66d54b24

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,6 +1,6 @@
-class CreateStudentsTable < ActiveRecord::Migration[5.0]
+class CreateStudents < ActiveRecord::Migration[5.0]
   def change
-    create_table :students_tables do |t|
+    create_table :students do |t|
       t.string :first_name
       t.string :last_name
 
